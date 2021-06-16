50a40720ea6bced5f7b84aa86351c07fb25338a5
DavidSDelaRosa <davidserranodelarosa@gmail.com>
2021-06-16 12:33:26 +0200

    Fase 2 del Nivel 1: Agregada la funcion 'hola' que puede recibir un nombre en el path

--- src/main/java/springBootInitialDemo/App.java
+++ src/main/java/springBootInitialDemo/App.java
@@ -4,13 +4,22 @@
 package springBootInitialDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class App {
 
     public static void main(String[] args) {
 
         SpringApplication.run(App.class, args);
+    }
+    @GetMapping("/")
+    public String hola(@RequestParam(value="nombre", defaultValue = "World") String nombre){
+        System.out.println("Hola!");
+        return String.format("Hola %s!", nombre);
     }
 }
